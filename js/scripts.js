8dc1617ec81883ff216e3ee95b05c6a48315bb54
Amy Churchwell and Lew Davidson <amy@>
2017-11-06 09:58:44 -0800

    add initial js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Mushroom (color) {
+  this.color = color;
+  this.shape = shape;
+  this.group = group;
+};
+
+
+$(document).ready(function() {
+  $(".btn-info").click(function(event) {
+    $("#results").show();
+    var yourMushroom = new Mushroom(color, shape,);
+  });
+});
