e3aec795ef97166c010def175166c2e74c466941
Amy Churchwell and Lew Davidson <amy@>
2017-11-06 14:47:38 -0800

    debug display filtered results

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,39 @@
-function Mushroom (color, shape, group) {
+function Mushroom (name, color, shape, group) {
+  this.name = name;
   this.color = color;
   this.shape = shape;
   this.group = group;
 };
 
-var buttonMush = new Mushroom ("brown", "round", "single");
-var henOfTheWoods = new Mushroom ("white", "ruffled", "grouped");
-var lobster = new Mushroom ("red", "ruffled", "single");
+var buttonMush = new Mushroom ("Button", "brown", "round", "single");
+var henOfTheWoods = new Mushroom ("Hen of The Woods", "white", "ruffled", "grouped");
+var lobster = new Mushroom ("Lobster", "red", "ruffled", "single");
+var blueChanterelle = new Mushroom ("Blue Chanterelle", "blue", "ruffled", "grouped");
 
 var mushrooms = [];
 mushrooms.push(buttonMush);
 mushrooms.push(henOfTheWoods);
 mushrooms.push(lobster);
+mushrooms.push(blueChanterelle);
 
 var filterCriteria = function(params){
-  var criteria = {};
+  var criteria = [];
   _.forEach(params, function(param){
     _.extend(criteria, param);
   });
   return criteria;
-}
+};
 
-var search = filterCriteria([{color: "red"}]);
-var filteredShrooms = _.filter(mushrooms, search);
+// var search = filterCriteria([{color: "red"}]);
+
 
 $(document).ready(function(){
-  $(".btn-info").click(function(event) {
-    $("#results").show();
-    console.log(filteredShrooms);
+  $("#blue").click(function(event) {
+    var search = filterCriteria([{shape: "ruffled"}]);
+    var filteredShrooms = _.filter(mushrooms, search);
+    for(var i = 0; i < filteredShrooms.length; i++){
+      $("#results").show().append(filteredShrooms[i].name + "<br>");
+    }
     event.preventDefault();
   });
 });
