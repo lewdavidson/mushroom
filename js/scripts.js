28a769b12209b8bcd9c55e6b2d21bb756e871d88
Amy Churchwell and Lew Davidson <amy@>
2017-11-06 10:11:15 -0800

    add initial prototype for color

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-function Mushroom (color) {
+function Mushroom (color, shape, group) {
   this.color = color;
   this.shape = shape;
   this.group = group;
 };
 
+Mushroom.prototype.colorCalc = function () {
+  if (this.color === "gold") {
+    alert("works");
+  }
+}
+
+var yourMushroom = new Mushroom();
 
 $(document).ready(function() {
   $(".btn-info").click(function(event) {
     $("#results").show();
-    var yourMushroom = new Mushroom(color, shape,);
+    yourMushroom.color = $(this).val();
+    yourMushroom.colorCalc();
+
   });
 });
