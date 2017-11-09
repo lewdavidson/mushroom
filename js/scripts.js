c026f3fb125594c92573255c322584253719f654
Lew <lewdavidson47@gmail.com>
2017-11-09 14:33:27 -0800

    fix results

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ function Mushroom (name, color, shape, group) {
   this.shape = shape;
   this.group = group;
 };
-
 // DONT FORGET TO PUSH NEW SHROOMS :) //
 var buttonMush = new Mushroom ("Button", "brown", "round", "single");
 var morell = new Mushroom ("Morell", "brown", "spear", "single");
@@ -24,8 +23,6 @@ var eryngii = new Mushroom ("Peurotus Eryngii", "brown", "flat", "grouped");
 var shiitake = new Mushroom ("Shiitake", "brown", "round", "single");
 var whiteBeech = new Mushroom ("White Beech", "white", "round", "grouped");
 var woodEar = new Mushroom ("Wood Ear", "brown", "fanned", "grouped");
-
-
 var mushrooms = [];
 mushrooms.push(buttonMush);
 mushrooms.push(henOfTheWoods);
@@ -44,8 +41,6 @@ mushrooms.push(eryngii);
 mushrooms.push(shiitake);
 mushrooms.push(whiteBeech);
 mushrooms.push(woodEar);
-
-
 var filterCriteria = function(params){
   var criteria = [];
   _.forEach(params, function(param){
@@ -53,13 +48,10 @@ var filterCriteria = function(params){
   });
   return criteria;
 };
-
 var searchFor = [];
 var searchParams = [];
 var filteredShrooms;
-
 //COLOR SELECTION//
-
 $(document).ready(function(){
   $("#submit").click(function(){
     $(".answers, .carouseldiv").show();
@@ -67,9 +59,6 @@ $(document).ready(function(){
   $("#top-arrow").click(function(){
     $("#jumbo").slideUp();
   });
-
-
-
   $("#blue").click(function(event) {
     searchFor.push({color: "blue"});
     searchParams = filterCriteria(searchFor);
@@ -77,7 +66,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#white").click(function(event) {
     searchFor.push({color: "white"});
     searchParams = filterCriteria(searchFor);
@@ -85,7 +73,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#gold").click(function(event) {
     searchFor.push({color: "gold"});
     searchParams = filterCriteria(searchFor);
@@ -93,7 +80,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#brown").click(function(event) {
     searchFor.push({color: "brown"});
     searchParams = filterCriteria(searchFor);
@@ -101,9 +87,7 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
 //SHAPE SELECTION//
-
   $("#round").click(function(event) {
     searchFor.push({shape: "round"});
     searchParams = filterCriteria(searchFor);
@@ -111,7 +95,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#ruffled").click(function(event) {
     searchFor.push({shape: "ruffled"});
     searchParams = filterCriteria(searchFor);
@@ -119,7 +102,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#spear").click(function(event) {
     searchFor.push({shape: "spear"});
     searchParams = filterCriteria(searchFor);
@@ -127,7 +109,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#flat").click(function(event) {
     searchFor.push({shape: "flat"});
     searchParams = filterCriteria(searchFor);
@@ -135,7 +116,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#concave").click(function(event) {
     searchFor.push({shape: "concave"});
     searchParams = filterCriteria(searchFor);
@@ -143,7 +123,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
 //GROUP SELECTION//
   $("#single").click(function(event) {
     searchFor.push({group: "single"});
@@ -152,7 +131,6 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
   $("#grouped").click(function(event) {
     searchFor.push({group: "grouped"});
     searchParams = filterCriteria(searchFor);
@@ -160,86 +138,64 @@ $(document).ready(function(){
     console.log(filteredShrooms);
     event.preventDefault();
   });
-
         // WALL OF MUSHROOM CARDS! (dry this code up?)//
-
-        $("#bottom-arrow").click(function(event){
-          $(".main-page").slideUp();
+        $("#submit").click(function(event){
           $("#answers").show();
           for(var i = 0; i < filteredShrooms.length; i++){
-
             if(filteredShrooms.includes(chanterelle)){
               $("#chanterelle").show();
             };
-
             if(filteredShrooms.includes(morell)){
               $("#morell").show();
             };
-
             if(filteredShrooms.includes(henOfTheWoods)){
               $("#hen").show();
             };
-
             if(filteredShrooms.includes(buttonMush)){
               $("#button").show();
             };
-
             if(filteredShrooms.includes(turkeyTail)){
               $("#turkeytail").show();
             };
-
             if(filteredShrooms.includes(lionsMane)){
               $("#lionsmane").show();
             };
-
             if(filteredShrooms.includes(lobster)){
               $("#lobster").show();
             };
-
             if(filteredShrooms.includes(blueChanterelle)){
               $("#bluechanterelle").show();
             };
-
             if(filteredShrooms.includes(indigoMilkcap)){
               $("#indigomilkcap").show();
             };
-
             if(filteredShrooms.includes(jackOLantern)){
-              $("#jackolantern").show();
+              $("#jackcard").show();
             };
-
             if(filteredShrooms.includes(enoki)){
               $("#enoki").show();
             };
-
             if(filteredShrooms.includes(cloudEar)){
               $("#cloudear").show();
             };
-
             if(filteredShrooms.includes(honshimeji)){
               $("#honshimeji").show();
             };
-
             if(filteredShrooms.includes(matsutake)){
               $("#matsutake").show();
             };
-
             if(filteredShrooms.includes(eryngii)){
               $("#peurotuseryngii").show();
             };
-
             if(filteredShrooms.includes(shiitake)){
               $("#shiitake").show();
             };
-
             if(filteredShrooms.includes(whiteBeech)){
               $("#whitebeech").show();
             };
-
             if(filteredShrooms.includes(woodEar)){
               $("#woodear").show();
             };
-
           };
         event.preventDefault();
       });
